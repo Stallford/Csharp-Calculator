c3c3854299a887abf27f7737d367ebd5365acb10
Stallford <jhonnquinto@hotmail.com.ar>
2024-01-15 23:34:02 -0500

    Subido el proyecto completo como prueba

---
+++ Calculadoratarea/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace Calculadoratarea
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            allclear = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            txtres = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Cursor = Cursors.Hand;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(11, 282);
+            button1.Name = "button1";
+            button1.Size = new Size(59, 53);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Cursor = Cursors.Hand;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(76, 282);
+            button2.Name = "button2";
+            button2.Size = new Size(59, 53);
+            button2.TabIndex = 1;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Cursor = Cursors.Hand;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(141, 282);
+            button3.Name = "button3";
+            button3.Size = new Size(59, 53);
+            button3.TabIndex = 2;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Cursor = Cursors.Hand;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(11, 223);
+            button4.Name = "button4";
+            button4.Size = new Size(59, 53);
+            button4.TabIndex = 3;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Cursor = Cursors.Hand;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(76, 223);
+            button5.Name = "button5";
+            button5.Size = new Size(59, 53);
+            button5.TabIndex = 4;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Cursor = Cursors.Hand;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button6.Location = new Point(141, 223);
+            button6.Name = "button6";
+            button6.Size = new Size(59, 53);
+            button6.TabIndex = 5;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Cursor = Cursors.Hand;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button7.Location = new Point(11, 164);
+            button7.Name = "button7";
+            button7.Size = new Size(59, 53);
+            button7.TabIndex = 8;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Cursor = Cursors.Hand;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button8.Location = new Point(76, 164);
+            button8.Name = "button8";
+            button8.Size = new Size(59, 53);
+            button8.TabIndex = 7;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Cursor = Cursors.Hand;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button9.Location = new Point(141, 164);
+            button9.Name = "button9";
+            button9.Size = new Size(59, 53);
+            button9.TabIndex = 6;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Cursor = Cursors.Hand;
+            button10.FlatStyle = FlatStyle.Flat;
+            button10.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button10.Location = new Point(11, 341);
+            button10.Name = "button10";
+            button10.Size = new Size(124, 53);
+            button10.TabIndex = 9;
+            button10.Text = "0";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Cursor = Cursors.Hand;
+            button11.FlatStyle = FlatStyle.Flat;
+            button11.Font = new Font("Century Gothic", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button11.Location = new Point(141, 341);
+            button11.Name = "button11";
+            button11.Size = new Size(59, 53);
+            button11.TabIndex = 10;
+            button11.Text = ".";
+            button11.TextAlign = ContentAlignment.TopCenter;
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.SteelBlue;
+            button12.Cursor = Cursors.Hand;
+            button12.FlatStyle = FlatStyle.Flat;
+            button12.Font = new Font("Century Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button12.Location = new Point(206, 282);
+            button12.Name = "button12";
+            button12.Size = new Size(59, 112);
+            button12.TabIndex = 11;
+            button12.Text = "=";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += button12_Click;
+            // 
+            // allclear
+            // 
+            allclear.BackColor = Color.SteelBlue;
+            allclear.Cursor = Cursors.Hand;
+            allclear.FlatStyle = FlatStyle.Flat;
+            allclear.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            allclear.Location = new Point(206, 223);
+            allclear.Name = "allclear";
+            allclear.Size = new Size(59, 53);
+            allclear.TabIndex = 12;
+            allclear.Text = "AC";
+            allclear.UseVisualStyleBackColor = false;
+            allclear.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.SteelBlue;
+            button14.Cursor = Cursors.Hand;
+            button14.FlatStyle = FlatStyle.Flat;
+            button14.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button14.Location = new Point(206, 164);
+            button14.Name = "button14";
+            button14.Size = new Size(59, 53);
+            button14.TabIndex = 13;
+            button14.Text = "DEL";
+            button14.UseVisualStyleBackColor = false;
+            button14.Click += button14_Click;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.SteelBlue;
+            button15.Cursor = Cursors.Hand;
+            button15.FlatStyle = FlatStyle.Flat;
+            button15.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button15.ForeColor = SystemColors.ActiveCaptionText;
+            button15.Location = new Point(11, 105);
+            button15.Name = "button15";
+            button15.Size = new Size(59, 53);
+            button15.TabIndex = 14;
+            button15.Text = "+";
+            button15.UseVisualStyleBackColor = false;
+            button15.Click += button15_Click;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.SteelBlue;
+            button16.Cursor = Cursors.Hand;
+            button16.FlatStyle = FlatStyle.Flat;
+            button16.Font = new Font("Century Gothic", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button16.ForeColor = SystemColors.ActiveCaptionText;
+            button16.Location = new Point(76, 105);
+            button16.Name = "button16";
+            button16.Size = new Size(59, 53);
+            button16.TabIndex = 15;
+            button16.Text = "-";
+            button16.TextAlign = ContentAlignment.TopCenter;
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += button16_Click;
+            // 
+            // button17
+            // 
+            button17.BackColor = Color.SteelBlue;
+            button17.Cursor = Cursors.Hand;
+            button17.FlatStyle = FlatStyle.Flat;
+            button17.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button17.ForeColor = SystemColors.ActiveCaptionText;
+            button17.Location = new Point(141, 105);
+            button17.Name = "button17";
+            button17.Size = new Size(59, 53);
+            button17.TabIndex = 16;
+            button17.Text = "x";
+            button17.UseVisualStyleBackColor = false;
+            button17.Click += button17_Click;
+            // 
+            // button18
+            // 
+            button18.BackColor = Color.SteelBlue;
+            button18.Cursor = Cursors.Hand;
+            button18.FlatStyle = FlatStyle.Flat;
+            button18.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button18.ForeColor = SystemColors.ActiveCaptionText;
+            button18.Location = new Point(206, 105);
+            button18.Name = "button18";
+            button18.Size = new Size(59, 53);
+            button18.TabIndex = 17;
+            button18.Text = "/";
+            button18.UseVisualStyleBackColor = false;
+            button18.Click += button18_Click;
+            // 
+            // txtres
+            // 
+            txtres.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            txtres.Location = new Point(11, 23);
+            txtres.Name = "txtres";
+            txtres.ReadOnly = true;
+            txtres.Size = new Size(254, 61);
+            txtres.TabIndex = 18;
+            txtres.TextAlign = HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(275, 405);
+            Controls.Add(txtres);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(allclear);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Calculadora";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button allclear;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private TextBox txtres;
+    }
+}
---
+++ Calculadoratarea/Form1.cs
@@ -0,0 +1,220 @@
+namespace Calculadoratarea
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string op = "";
+        double n1 = 0;
+        double n2 = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtres.Text == "0")
+            {
+                txtres.Text = "1";
+            }
+            else
+            {
+                txtres.Text = txtres.Text + "1";
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            txtres.Text = "0";
+            n1 = 0;
+            n2 = 0;
+            op = "0";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (txtres.TextLength == 1)
+            {
+                txtres.Text = "0";
+            }
+            else
+            {
+                txtres.Text = txtres.Text.Substring(0, txtres.TextLength - 1);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtres.Text == "0")
+            {
+                txtres.Text = "2";
+            }
+            else
+            {
+                txtres.Text = txtres.Text + "2";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (txtres.Text == "0")
+            {
+                txtres.Text = "3";
+            }
+            else
+            {
+                txtres.Text = txtres.Text + "3";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (txtres.Text == "0")
+            {
+                txtres.Text = "4";
+            }
+            else
+            {
+                txtres.Text = txtres.Text + "4";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (txtres.Text == "0")
+            {
+                txtres.Text = "5";
+            }
+            else
+            {
+                txtres.Text = txtres.Text + "5";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (txtres.Text == "0")
+            {
+                txtres.Text = "6";
+            }
+            else
+            {
+                txtres.Text = txtres.Text + "6";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (txtres.Text == "0")
+            {
+                txtres.Text = "7";
+            }
+            else
+            {
+                txtres.Text = txtres.Text + "7";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (txtres.Text == "0")
+            {
+                txtres.Text = "8";
+            }
+            else
+            {
+                txtres.Text = txtres.Text + "8";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (txtres.Text == "0")
+            {
+                txtres.Text = "9";
+            }
+            else
+            {
+                txtres.Text = txtres.Text + "9";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txtres.Text = txtres.Text + "0";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            txtres.Text = txtres.Text + ",";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            op = "+";
+            n1 = Convert.ToDouble(txtres.Text);
+            txtres.Text = "0";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            op = "-";
+            n1 = Convert.ToDouble(txtres.Text);
+            txtres.Text = "0";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            op = "*";
+            n1 = Convert.ToDouble(txtres.Text);
+            txtres.Text = "0";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            op = "/";
+            n1 = Convert.ToDouble(txtres.Text);
+            txtres.Text = "0";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                n2 = Convert.ToDouble(txtres.Text);
+                if (op != null)
+                {
+                    switch (op)
+                    {
+                        case "+":
+                            txtres.Text = $"{n1 + n2}";
+                            break;
+                        case "-":
+                            txtres.Text = $"{n1 - n2}";
+                            break;
+                        case "*":
+                            txtres.Text = $"{n1 * n2}";
+                            break;
+                        case "/":
+                            if (n2 != 0)
+                            {
+                                txtres.Text = $"{n1 / n2}";
+                            }
+                            else
+                            {
+                                throw new DivideByZeroException("Error");
+                            }
+                            break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                txtres.Text = "Error";
+            }
+        }
+    }
+}
